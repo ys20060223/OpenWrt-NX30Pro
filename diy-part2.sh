dd9686c51fcdde98c260b1eb81ee61cd84c39a81
Rainy night look at flowers <110514628+ys20060223@users.noreply.github.com>
2024-10-07 00:03:30 +0800

    diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,36 +12,9 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-
-# 修改默认主机名
-#sed -i '/uci commit system/i\uci set system.@system[0].hostname='Soft_Router'' package/lean/default-settings/files/zzz-default-settings
- 
-# 加入编译者信息
-#sed -i "s/OpenWrt /Kinoko build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
-# 安装前置 mosdns 5.3.1
-# rm -rf feeds/packages/lang/golang
-# git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
-
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
-# 删除冲突
-
-rm -rf feeds/packages/net/mosdns
-rm -rf feeds/packages/net/v2ray-geodata
-rm -rf package/feeds/packages/mosdns
-rm -rf package/feeds/packages/v2ray-geodata
-rm -rf feeds/luci/applications/luci-app-openclash
-rm -rf package/feeds/luci/luci-app-openclash
-
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-#git clone https://github.com/sbwml/v2ray-geodata feeds/packages/net/v2ray-geodata
-git clone --depth=1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
-
